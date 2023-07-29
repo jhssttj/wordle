30569324fa59c34bec4f46998e4c1c7204e9ea95
Kevin Lee <jhssttj@hotmail.com>
2023-07-29 16:31:42 -0600

    Test fetch request of api for random 5 letter word

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import {useEffect, useState} from "react";
+
 
 function App() {
+
+  useEffect(()=> {
+    fetch("https://random-word-api.herokuapp.com/word?length=5")
+      .then(res=>res.json())
+      .then(json => {
+        console.log(json)
+      })
+  }, )
+
   return (
     <div className="App">
+      <h1>Working?</h1>
     </div>
   );
 }
