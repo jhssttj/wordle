7276ca0397175be6082d49a138843f8ba3ba6184
Kevin Lee <jhssttj@hotmail.com>
2023-07-29 21:44:07 -0600

    Render solution word test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import {useEffect, useState} from "react";
+import {useState, useEffect} from "react";
 
 
 function App() {
 
+  const [answer, setAnswer] = useState(null);
+
   useEffect(() => {
     fetch("https://random-word-api.vercel.app/api?words=1&length=5")
       .then((response) => {return response.json()})
-      .then((word) => {console.log(word)})
-  })
+      .then((word) => {setAnswer(word)})
+  }, [])
 
   return (
     <div className="App">
-      <h1>Working?</h1>
+      <h1>{answer}</h1>
     </div>
   );
 }
