acee3140f298d4dfc8f03b0882e4aa0c21162cb7
Kevin Lee <jhssttj@hotmail.com>
2023-07-30 19:39:20 -0600

    Add window event listener to determine the letter typed by user

---
+++ src/components/WordleMain.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react'
+import useWordle from '../hooks/useWordle';
+
+export function WordleMain({answer}) {
+  const {currentGuess, handleKeyup} = useWordle(answer);
+  
+  useEffect(()=> {
+    window.addEventListener('keyup', handleKeyup);
+
+    return () => {window.removeEventListener('keyup',handleKeyup)}
+  }, [handleKeyup])
+
+  return (
+    <div>WordleMain</div>
+  )
+}
+
+export default WordleMain;
