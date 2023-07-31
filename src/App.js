f27a326a6474c7defdb0499b54c4a07f0048ea36
Kevin Lee <jhssttj@hotmail.com>
2023-07-30 19:14:29 -0600

    Initialize hook page for game mechanics and logic

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {useState, useEffect} from "react";
 
 function App() {
 
-  const [answer, setAnswer] = useState(null);
+  const [answer, setAnswer] = useState("");
 
   useEffect(() => {
     fetch("https://random-word-api.vercel.app/api?words=1&length=5")
       .then((response) => {return response.json()})
-      .then((word) => {setAnswer(word)})
+      .then((answer) => {setAnswer(answer)})
   }, [])
 
   return (
