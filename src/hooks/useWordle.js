d87a687daa1b6d71d3ab1eeae7a322c5e3bac57f
Kevin Lee <jhssttj@hotmail.com>
2023-07-31 15:34:11 -0600

    Add handle key for enter and its condition to block if not allowed to answer

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -8,16 +8,37 @@ const useWordle = (solution) => {
   const [history, setHistory] = useState([]);
   const [isCorrect, setIsCorrect] = useState(false);
 
-  const formatGuess = () => {}
+  const formatGuess = () => {
+    console.log('format guess')
+  }
 
   const addNewGuess = () => {}
 
   const handleKeyup = ({key}) => {
+    if (key ==='Enter') {
+      if (turn > 5) {
+        console.log("Turn exceed")
+        return;
+      }
+      if (history.includes(currentGuess)) {
+        console.log("Duplicate entry")
+        return;
+      }
+
+      if (currentGuess.length !== 5) {
+        console.log('Word isnt 5 chara length')
+        return;
+      }
+
+      formatGuess();
+    }
+
     if (key === 'Backspace') {
       setCurrentGuess((prev)=> {
         return prev.slice(0,-1);
       })
     }
+
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
         setCurrentGuess((prev)=>{
