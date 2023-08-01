612aa766ed94a5776c00d5f461fa1fc0b90278fd
Kevin Lee <jhssttj@hotmail.com>
2023-07-31 20:07:39 -0600

    Start over due to react bug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import {useState, useEffect} from "react";
-import {WordleMain} from './components/WordleMain';
+
 
 
 function App() {
-
-  const [answer, setAnswer] = useState("");
-
-  useEffect(() => {
-    fetch("https://random-word-api.vercel.app/api?words=1&length=5")
-      .then((response) => {return response.json()})
-      .then((answer) => {setAnswer(answer[0])})
-  }, [])
+  // useEffect(() => {
+  //   fetch("https://random-word-api.vercel.app/api?words=1&length=5")
+  //     .then((response) => {return response.json()})
+  //     .then((answer) => {setAnswer(answer[0])})
+  // }, [])
 
 
   return (
     <div className="App">
-      <h1>{answer}</h1>
-      <WordleMain answer={answer}/>
+
     </div>
   );
 }
