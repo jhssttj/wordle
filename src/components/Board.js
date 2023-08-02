d14031e73ae81c61b8ad6cff34934924cb5d9ebd
Kevin Lee <jhssttj@hotmail.com>
2023-08-01 18:42:32 -0600

    CSS so that board size expands with number of turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,29 +7,35 @@ function Board() {
   const [board, setBoard] = useState(boardDefault);
 
   return (
-    <div className="board border-2 border-black h-[550px] w-[450px] flex flex-col m-8 ">
-      <div className="row">
+    <div className="board border-2 border-black h-auto w-auto flex flex-col m-8 ">
+      <div className="row border-2 border-black flex justify-around h-full w-full">
         <Letter letterPos={0} attemptVal={0}/>
         <Letter letterPos={1} attemptVal={0}/>
         <Letter letterPos={2} attemptVal={0}/>
         <Letter letterPos={3} attemptVal={0}/>
         <Letter letterPos={4} attemptVal={0}/>
       </div>
-      <div className="row">
+      <div className="row border-2 border-black flex justify-around h-full">
         <Letter letterPos={0} attemptVal={1}/>
         <Letter letterPos={1} attemptVal={1}/>
         <Letter letterPos={2} attemptVal={1}/>
         <Letter letterPos={3} attemptVal={1}/>
         <Letter letterPos={4} attemptVal={1}/>
       </div>
-      <div className="row">
+      <div className="row border-2 border-black flex justify-around h-full">
         <Letter letterPos={0} attemptVal={2}/>
         <Letter letterPos={1} attemptVal={2}/>
         <Letter letterPos={2} attemptVal={2}/>
         <Letter letterPos={3} attemptVal={2}/>
         <Letter letterPos={4} attemptVal={2}/>
       </div>
-      <div className="row"></div>
+      <div className="row border-2 border-black flex justify-around h-full">
+        <Letter letterPos={0} attemptVal={2}/>
+        <Letter letterPos={1} attemptVal={2}/>
+        <Letter letterPos={2} attemptVal={2}/>
+        <Letter letterPos={3} attemptVal={2}/>
+        <Letter letterPos={4} attemptVal={2}/>
+      </div>
       <div className="row"></div>
     </div>
   )
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Letter({letterPos, attemptValue}) {
   return (
-    <div className="letter">Letter</div>
+    <div className="letter border-2 border-black grid h-full m-[5px] min-h-[100px] min-w-[100px] grid">Letter</div>
   )
 }
 
