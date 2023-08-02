d0041b8d09e279736920bfb3aea7d10746e2e89c
Kevin Lee <jhssttj@hotmail.com>
2023-08-01 21:07:06 -0600

    Initalize auto render of board rows and letters

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Letter from './Letter';
+import {AppContext} from '../App'
 
 function Board() {
+  const {board} = useContext(AppContext);
+
+  const renderBoardContent = () => {
+    for (let i = 0; i < board.length; i++) {
+      console.log(board[i])
+    }
+
+    return (<div className="flex justify-around">
+        Test
+      </div>)
+  }
 
   return (
     <div className="board border-2 border-black h-auto w-auto flex flex-col m-8 ">
@@ -34,6 +46,7 @@ function Board() {
         <Letter letterPos={4} attemptVal={3}/>
       </div>
       <div className="row"></div>
+      {renderBoardContent()}
     </div>
   )
 }
