b4a1025d9302c81fad518e2dc83258b5c9610c92
Kevin Lee <jhssttj@hotmail.com>
2023-08-01 21:25:13 -0600

    Finish function for rendering row and letter to board

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,36 +3,29 @@ import Letter from './Letter';
 import {AppContext} from '../App'
 
 function Board() {
+
   const {board} = useContext(AppContext);
 
-  const renderLetter = (attemptVal) => {
-    const boardRow = board[attemptVal];
-    console.log(boardRow)
-    const letters = boardRow.map((i,y)=> {
-      console.log(boardRow[i])
-      return <Letter key={y} letter={boardRow[i]}/>
+  //Render rows for letters to go in
+  const renderRows = () => {
+    const rows = board.map((row, rowIndex) => {
+      return <div key={rowIndex} className="flex justify-content">{renderLetters(row)}</div>
+    })
+    
+    return rows;
+  }
+  //Render letters of the current row
+  const renderLetters = (row) => {
+    const letters = row.map((letter,index)=> {
+      return <Letter key={index} letter={letter}/>
     })
     return letters;
   }
 
-  const renderBoardContent = () => {
-    for (let i = 0; i < board.length; i++) {
-      
-    }
-
-    return (<div className="flex justify-around">
-        Test
-      </div>)
-  }
-
   return (
     <div className="board border-2 border-black h-auto w-auto flex flex-col m-8 ">
-      <div className="flex justify-around">
-        {renderLetter(0)}
-      </div>
-
+        {renderRows()}
       <div className="row"></div>
-      {renderBoardContent()}
     </div>
   )
 }
