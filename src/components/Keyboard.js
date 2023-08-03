6b78af569c6d958ecc242d04823cfe1474727b7b
Kevin Lee <jhssttj@hotmail.com>
2023-08-02 18:52:54 -0600

    Add array data for keyboard layout

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function Keyboard() {
+  const keyRow1 = ["Q","W","E","R","T","Y","U","I","O","P"];
+  const keyRow2 = ["A","S","D","F","G","H","J","K","L"];
+  const keyRow3 = ["Z","X","C","V","B","N","M"];
+
   return (
-    <div>Keyboard</div>
+    <div className="">
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+      
+    </div>
   )
 }
 
