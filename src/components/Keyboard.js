20cb2737c6f70080a0808d21fe5c2a07708a7d1b
Kevin Lee <jhssttj@hotmail.com>
2023-08-03 18:55:16 -0600

    Add function so you can also type in your word with your keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useEffect, useContext, useCallback} from 'react'
 import {keyRow1, keyRow2, keyRow3} from '../Data';
 import Key from './Key';
+import {AppContext} from '../App'
 
 function Keyboard() {
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === "Enter") {
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      keyRow1.forEach((key) => {
+        if (event.key.toUpperCase() === key) {
+          onSelectLetter(key)
+        }
+      })
+      keyRow2.forEach((key) => {
+        if (event.key.toUpperCase() === key) {
+          onSelectLetter(key)
+        }
+      })
+      keyRow3.forEach((key) => {
+        if (event.key.toUpperCase() === key) {
+          onSelectLetter(key)
+        }
+      })
+    }
+  }, [onEnter,onDelete,onSelectLetter])
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyboard)
+    return () => window.removeEventListener('keydown', handleKeyboard)
+  }, [handleKeyboard])
 
   const renderKeys = (keyRow) => {
     return keyRow.map((key, index) => {
@@ -11,7 +42,7 @@ function Keyboard() {
   }
 
   return (
-    <div className="border-2 border-black flex flex-col items-center">
+    <div className="border-2 border-black flex flex-col items-center" onKeyDown={handleKeyboard}>
       <div className="flex space-evenly">
         {renderKeys(keyRow1)}
       </div>
