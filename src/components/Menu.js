7069d47eae31de31d4f712d5dc9bfea3d34ddbeb
Kevin Lee <jhssttj@hotmail.com>
2023-08-10 18:52:32 -0600

    NPM Install font awesome icons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,21 @@
 import React, {useContext} from 'react';
-import Letter from './Letter';
-import {AppContext} from '../App'
+import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa'
 
 function Menu() {
 
   return (
     <div className="">
-        
+        <div>
+          <FaArrowAltCircleLeft/>
+          Div for setting word length
+          <FaArrowAltCircleRight/>
+        </div>
+        <div>
+          <FaArrowAltCircleLeft/>
+          Div for setting attempt number
+          <FaArrowAltCircleRight/>
+        </div>
+        <button>PLAY</button>
     </div>
   )
 }
