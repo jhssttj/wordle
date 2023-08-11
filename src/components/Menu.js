aac106f0a5ca6f20e1eb0ba5b4edd7d357c14f41
Kevin Lee <jhssttj@hotmail.com>
2023-08-10 18:55:33 -0600

    Format div to make word length and attempt menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,28 @@
 import React, {useContext} from 'react';
+import {AppContext} from '../App'
 import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa'
 
 function Menu() {
 
+  const { attempts, setAttempts, ansLength, setAnsLength } = useContext(AppContext);
+  
   return (
     <div className="">
-        <div>
-          <FaArrowAltCircleLeft/>
-          Div for setting word length
-          <FaArrowAltCircleRight/>
+        <div className="">
+          <h1>Word Length</h1>
+          <div className="flex">
+            <FaArrowAltCircleLeft/>
+            <p>{ansLength}</p>
+            <FaArrowAltCircleRight/>
+          </div>
         </div>
-        <div>
-          <FaArrowAltCircleLeft/>
-          Div for setting attempt number
-          <FaArrowAltCircleRight/>
+        <div className="">
+          <h1>Attempt Number</h1>
+          <div className="flex">
+            <FaArrowAltCircleLeft/>
+            <p>{attempts}</p>
+            <FaArrowAltCircleRight/>
+          </div>
         </div>
         <button>PLAY</button>
     </div>
