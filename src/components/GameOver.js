75695725def08145d57a5114462e8307d410933e
Kevin Lee <jhssttj@hotmail.com>
2023-08-14 18:55:47 -0600

    CSS game over screen

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -5,14 +5,15 @@ function GameOver() {
 
   const {gameOver, answer, inputPosition, restartFunction} = useContext(AppContext);
   const currentAttempt = inputPosition.attempt;
-  const message = gameOver.guessedWord? "Correct":"Incorrect"
+  const message = gameOver.guessedWord? "You Won!":"Game Over"
 
   return (
-    <div className="border-2 border-black flex flex-col items-center">
-      <h3 className="text-xl font-bold">{message}</h3>
-      <h1>Correct word was {answer}</h1>
+    <div className="bg-lgray rounded-lg p-2 flex flex-col items-center">
+      <h3 className="font-bold sm:text-4xl text-2xl">{message}</h3>
+      <h1 className="sm:text-2xl">Correct word was {answer}</h1>
       {gameOver.guessedWord && <h1>You guessed in {currentAttempt} attempts</h1>}
-      <button onClick={()=> restartFunction()}>Restart?</button>
+      <button className=" bg-green text-xlgreen rounded-lg p-2 font-bold hover:opacity-90 my-4 sm:text-2xl"
+        onClick={()=> restartFunction()}>Restart</button>
       </div>
   )
 }
